66090e2d73cfae743e2ca013756b820fe56a990a
Ko-s-y <song.1209@icloud.com>
2022-09-20 23:21:01 +0900

    ログアウト機能実装 #3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new' # ログイン画面
   post 'login', to: 'sessions#create' # ログイン処理
+  delete 'logout', to: 'sessions#destroy' # ログアウト処理
 
   namespace :admin do
     resources :users
