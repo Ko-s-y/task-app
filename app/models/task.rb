cf96ec687c7d1add6f2d7f6375b548c9f462c708
Ko-s-y <song.1209@icloud.com>
2022-09-20 18:40:25 +0900

    ,をtask.nameに含められないように設定 #2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  def validate_name_not_including_comma
+    if name&.include?(',')
+      errors.add(:name, 'にカンマを含める事はできません')
+    end
+  end
 end
