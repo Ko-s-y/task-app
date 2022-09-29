b0f4a9cc512147e5665b3f3876197d7af88ef973
Ko-s-y <song.1209@icloud.com>
2022-09-30 01:13:01 +0900

    react導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails, { loadCSPNonce } from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import React from "react";
+import ReactDom from "react-dom";
 
 Rails.start()
 Turbolinks.start()
